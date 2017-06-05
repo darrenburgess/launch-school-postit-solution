2354ca98e6f2bf9314c151e19e6f589154db3885
Darren Burgess <darrentburgess@gmail.com>
2017-06-04 21:57:52 -0400

    add post templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: [:show, :edit, :update]
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
