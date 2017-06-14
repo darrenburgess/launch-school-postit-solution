54c8bf47ea6c732e34da119a2007e2d85f93ad23
Darren Burgess <darrentburgess@gmail.com>
2017-06-13 22:34:49 -0400

    add crud actions for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,53 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category was Created"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category was Updated"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = "Category was Deleted"
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
