75e661c5ea62c6fd9f44f151a7a07bc509855225
Darren Burgess <darrentburgess@gmail.com>
2017-06-27 17:26:21 -0400

    add votes and basic ui for voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627111155) do
+ActiveRecord::Schema.define(version: 20170627135210) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 20170627111155) do
     t.string   "password_digest"
     t.string   "first_name"
     t.string   "last_name"
+  end
+
+  create_table "votes", force: true do |t|
+    t.boolean  "vote"
+    t.integer  "user_id"
+    t.string   "voteable_type"
+    t.integer  "voteable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
