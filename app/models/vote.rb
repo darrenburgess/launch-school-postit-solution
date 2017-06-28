adaa6e880168a4cc7c897e3fedc0d92172ec470e
Darren Burgess <darrentburgess@gmail.com>
2017-06-28 19:53:52 -0400

    remove comment

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
   
-  # the problem here appears to be that this validation
-  # is not specific enough to handle votes with duplicate
-  # user_id's from both the Post and Comment models
   validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
 end
