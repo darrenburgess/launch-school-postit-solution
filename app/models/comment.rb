5125feb4cb2a1bc995fdd95c3a09bdb21cbe9ad6
Darren Burgess <darrentburgess@gmail.com>
2017-06-29 07:42:44 -0400

    move total votes method to voteable concern

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
-  has_many :votes, as: :voteable
 
   validates :body, presence: true
   validates :author, presence: true
 
-  def total_votes
-    up_votes = self.votes.where(vote: true).size
-    down_votes = self.votes.where(vote: false).size
-    up_votes - down_votes
-  end
+  include Voteable
 end
