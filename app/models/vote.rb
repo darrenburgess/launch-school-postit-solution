22765834ca0e7441c06f63ebf82021ddf0be96d3
Darren Burgess <darrentburgess@gmail.com>
2017-06-29 07:42:08 -0400

    add validations to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :voteable, polymorphic: true
   
-  validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
+  validates_uniqueness_of :creator, scope: [:voteable_id, :voteable_type]
 end
