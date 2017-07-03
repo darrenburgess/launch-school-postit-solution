381e6887da46477b85522f9f7c89abc0d8f74ba2
Darren Burgess <darrentburgess@gmail.com>
2017-07-03 11:24:48 -0400

    improve slugging

--- app/models/sluggable.rb
+++ app/models/sluggable.rb
@@ -2,17 +2,24 @@ module Sluggable
   extend ActiveSupport::Concern
 
   included do
-    after_validation :create_slug
+    before_save :generate_slug
   end
 
-  def create_slug
+  def generate_slug
     if self.class.name == "Post"
-      self.slug = self.title.gsub(" ", "-")
+      self.slug = create_slug(title)
     elsif self.class.name == "User"
-      self.slug = self.username.gsub(" ", "-")
+      self.slug = create_slug(username)
     elsif self.class.name == "Category"
-      self.slug = self.name.gsub(" ", "-")
+      self.slug = create_slug(name)
     end
+  end
+
+  def create_slug(str)
+    str.strip.gsub(/\W/, "-")
+             .gsub(/-{2,}/, "-")
+             .gsub(/ {2,}/, " ")
+             .gsub(/^-|-$/, "")
   end
 
   def to_param
